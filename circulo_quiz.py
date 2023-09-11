86ca27c35647e1b798a547c02b1de6c28603e1c0
Luis david <david12.29@hotmail.com>
2023-09-11 17:54:44 -0500

    Mejore el codigo

--- circulo_quiz.py
+++ circulo_quiz.py
@@ -1,8 +1,8 @@
 import random
 import math
 
-mi_circulo = [0,1,2,3,4,5,6,7]
-vector_pasos=[]
+MI_CIRCULO = [0,1,2,3,4,5,6,7]
+VECTOR_PASOS=[]
 
 #Se busca obtener el nuevo índice que hace referencia a un número adyacente al número inicial.
 #Esto se logra mediante random.randint(1, 2). 
@@ -30,12 +30,12 @@ def condiciones(indece_circulo):
 def buscar():
     indece_circulo=0
     numero_pasos=0
-    numero_circulo=mi_circulo[0]
+    numero_circulo=MI_CIRCULO[0]
     encontrar=False
     while encontrar==False:
         numero_pasos+=1
         indece_circulo=condiciones(indece_circulo)
-        numero_circulo=mi_circulo[indece_circulo]
+        numero_circulo=MI_CIRCULO[indece_circulo]
         if(numero_circulo==0 ):
             encontrar=True
     return numero_pasos
@@ -43,24 +43,25 @@ def buscar():
 #Ahora, se busca obtener el número de pasos necesarios para volver al punto de partida,
 #es decir, al número cero.Esto se logra a través de un ciclo for que iterará hasta encontrar el cero, 
 #registrando el número de pasos en un vector.
-for iteracion in range(1, 100):
-    vector_pasos.append(buscar())
+NUMERO_ITERACIONES=100
+for iteracion in range(1,NUMERO_ITERACIONES):
+    VECTOR_PASOS.append(buscar())
     
 
 
-vector_pasos.sort()
-longitud = len(vector_pasos)
-media = sum(vector_pasos) / longitud
-valor_minimo=vector_pasos[0]
-valor_maximo=vector_pasos[longitud-1]
+VECTOR_PASOS.sort()
+longitud = len(VECTOR_PASOS)
+media = sum(VECTOR_PASOS) / longitud
+valor_minimo=VECTOR_PASOS[0]
+valor_maximo=VECTOR_PASOS[longitud-1]
 
 if longitud % 2 == 1:
-    numero_medio = vector_pasos[longitud // 2]
+    numero_medio = VECTOR_PASOS[longitud // 2]
 else:
-    numero_medio = (vector_pasos[longitud // 2 - 1] + vector_pasos[longitud // 2]) / 2
+    numero_medio = (VECTOR_PASOS[longitud // 2 - 1] + VECTOR_PASOS[longitud // 2]) / 2
 
-suma_cuadrados_diferencias = sum((x - media) ** 2 for x in vector_pasos)
-varianza = suma_cuadrados_diferencias / len(vector_pasos)
+suma_cuadrados_diferencias = sum((x - media) ** 2 for x in VECTOR_PASOS)
+varianza = suma_cuadrados_diferencias / len(VECTOR_PASOS)
 desviacion_estandar = math.sqrt(varianza)
 
 print("El número medio es:       ", numero_medio)
